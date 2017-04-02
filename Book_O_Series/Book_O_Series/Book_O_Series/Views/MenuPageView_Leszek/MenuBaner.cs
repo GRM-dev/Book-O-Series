c0c6c50a29446537b89e66d6ad25775e6c90cd20
Leszek <rhoninn11@gmail.com>
2017-04-02 11:29:42 +0200

    Menu options with scroll

--- Book_O_Series/Book_O_Series/Book_O_Series/Views/MenuPageView_Leszek/MenuBaner.cs
+++ Book_O_Series/Book_O_Series/Book_O_Series/Views/MenuPageView_Leszek/MenuBaner.cs
@@ -9,62 +9,75 @@ namespace Book_O_Series.Views
 {
     class MenuBaner:StackLayout
     {
-        static public Label _accuntData;
-        static public Label _options;
+       
         private bool _isOff;
         private bool _isAnimated;
 
 
         public MenuBaner()
         {
-            _isOff = true;
-            _isAnimated = false;
+            this._isOff = true;
+            this._isAnimated = false;
             this.IsVisible = false;
-            StackLayout stackLayout = new StackLayout();
-            stackLayout.Orientation = StackOrientation.Horizontal;
-            stackLayout.Children.Add(new Image {
+
+            Image image = new Image
+            {
                 Source = ImageSource.FromResource("Book_O_Series.Images.userIcon.png"),
                 HorizontalOptions = LayoutOptions.CenterAndExpand
-            });
-            stackLayout.Children.Add(new Label
-            {
-                Text = "SampeText",
-                FontSize = Device.GetNamedSize(NamedSize.Large, typeof(Label)),
-                HorizontalTextAlignment = TextAlignment.Start,
-                HorizontalOptions = LayoutOptions.CenterAndExpand
-            });
-            (stackLayout.Children[0] as Image).WidthRequest = (stackLayout.Children[1] as Label).FontSize;
-            (stackLayout.Children[0] as Image).HeightRequest = (stackLayout.Children[1] as Label).FontSize;
+            };
+            StackLayout stackLayout = new StackLayout();
+
+            stackLayout.Children.Add(new MenuOption(image,"name"));
+            stackLayout.Children.Add(new MenuOption(image, "option1"));
+            stackLayout.Children.Add(new MenuOption(image, "option2"));
+            stackLayout.Children.Add(new MenuOption(image, "option3"));
+            stackLayout.Children.Add(new MenuOption(image, "option4"));
+            stackLayout.Children.Add(new MenuOption(image, "option1"));
+            stackLayout.Children.Add(new MenuOption(image, "option2"));
+            stackLayout.Children.Add(new MenuOption(image, "option3"));
+            stackLayout.Children.Add(new MenuOption(image, "option4"));
+            stackLayout.Children.Add(new MenuOption(image, "option1"));
+            stackLayout.Children.Add(new MenuOption(image, "option2"));
+            stackLayout.Children.Add(new MenuOption(image, "option3"));
+            stackLayout.Children.Add(new MenuOption(image, "option4"));
 
             this.Orientation = StackOrientation.Vertical;
-            this.Children.Add(stackLayout);
+            this.Children.Add(new ScrollView { VerticalOptions = LayoutOptions.FillAndExpand, Content = stackLayout });
             this.VerticalOptions = LayoutOptions.Fill;
             this.HorizontalOptions = LayoutOptions.Fill;
             this.Padding = new Thickness(5, 5, 5, 5);
         }
+
+        // handler który odpowiada za pojawienie i zniknięcie menu
         public void onMenuButtonClick(Object sender, EventArgs e)
         {
-            // _globalGrid.Children[1].IsVisible = false;
 
             if (_isOff && !this._isAnimated)
             {
                 this.TranslationX = 600;
+                this.RotationY = 180;
                 this.IsVisible = true;
                 this._isAnimated = true;
                 Device.StartTimer(TimeSpan.FromMilliseconds(1), () => {
                     if (this.TranslationX > 0)
                     {
                         this.TranslationX -= 10 + this.TranslationX / 5;
+                        this.RotationY -= 3 + this.RotationY / 5;
                         return true;
                     }
                     else
+                    {
                         this.TranslationX = 0;
-                    this._isAnimated = false;
-                    return false;
+                        this.RotationY = 0;
+                        this._isAnimated = false;
+                        return false;
+                    }
+                    
                 });
 
                 _isOff = false;
             }
+
             if (!_isOff && !this._isAnimated)
             {
                 this._isAnimated = true;
@@ -72,12 +85,12 @@ namespace Book_O_Series.Views
                     if (this.TranslationX < 600)
                     {
                         this.TranslationX += 10 + this.TranslationX / 5;
+                        
                         return true;
                     }
                     else
                     {
                         this.IsVisible = false;
-                        this.TranslationX = 0;
                         this._isAnimated = false;
                         return false;
                     }
--- Book_O_Series/Book_O_Series/Book_O_Series/Views/MenuPageView_Leszek/MenuOption.cs
+++ Book_O_Series/Book_O_Series/Book_O_Series/Views/MenuPageView_Leszek/MenuOption.cs
@@ -9,12 +9,43 @@ using Xamarin.Forms;
 namespace Book_O_Series.Views
 {
     class MenuOption:StackLayout
-    {
+    {   //niedokończone
+
         public MenuOption(Image image,string text)
         {
-            StackLayout stackLayout = new StackLayout{
+            StackLayout stackLayout = new StackLayout
+            {
                 Orientation = StackOrientation.Horizontal,
             };
+            
+            Label label = new Label
+            {
+                Text = text,
+                FontSize = Device.GetNamedSize(NamedSize.Default, typeof(Label)),
+                HorizontalOptions = LayoutOptions.Center,
+            };
+
+            Image icon = new Image
+            {
+                Source = image.Source,
+            };
+            icon.HorizontalOptions = LayoutOptions.Center;
+            icon.VerticalOptions = LayoutOptions.Center;
+            icon.WidthRequest = label.FontSize;
+            icon.HeightRequest = label.FontSize;
+
+            BoxView separator = new BoxView
+            {
+                Color = Color.Gray,
+                HorizontalOptions = LayoutOptions.Fill,
+                HeightRequest = 2.0F,
+                
+            };
+
+            stackLayout.Children.Add(icon);
+            stackLayout.Children.Add(label);
+            this.Children.Add(stackLayout);
+            this.Children.Add(separator);
 
         }
     }
